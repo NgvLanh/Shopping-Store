00384422e8e17126563eaec5c49969632da1b0b5
NgvLanh <130779744+NgvLanh@users.noreply.github.com>
2024-06-15 10:19:43 +0700

    Update OrderProcessController.java

--- src/main/java/com/poly/controllers/client/OrderProcessController.java
+++ src/main/java/com/poly/controllers/client/OrderProcessController.java
@@ -3,14 +3,18 @@ package com.poly.controllers.client;
 import com.poly.entities.*;
 import com.poly.repositories.*;
 import com.poly.services.SessionService;
+import com.poly.services.VNPayService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -30,18 +34,16 @@ public class OrderProcessController {
     SessionService sessionService;
     @Autowired
     ProductItemRepository productItemRepository;
+    @Autowired
+    VNPayService vnPayService;
 
     @PostMapping("/order")
     public String order(Model model,
                         @RequestParam("total") Double total,
                         @RequestParam("payment-method") String paymentMethod,
+                        HttpServletRequest request,
                         RedirectAttributes redirectAttributes) {
         Payment payment = new Payment();
-        if (paymentMethod.equals("onl")) {
-            payment.setStatus("completed");
-        } else {
-            payment.setStatus("pending");
-        }
         payment.setAmount(total);
         payment.setMethod(paymentMethod);
         paymentRepository.save(payment);
@@ -53,6 +55,12 @@ public class OrderProcessController {
         order.setTotal(total);
         order.setPayment(payment);
         orderRepository.save(order);
+        List<Order> orders = orderRepository.findAll();
+        Collections.reverse(orders);
+        Order order1 = orders.get(0);
+
+        Long orderId = order1.getOrderId();
+//
         List<CartItem> cartItemList = cartItemRepository.findAll();
         for (CartItem cartItem : cartItemList) {
             OrderItem orderItem = new OrderItem();
@@ -65,12 +73,15 @@ public class OrderProcessController {
             productItemRepository.save(productItem);
             orderItemRepository.save(orderItem);
         }
+        if (paymentMethod.equals("onl")) {
+            String baseUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+            String vnpayUrl = vnPayService.createOrder(Integer.parseInt(String.valueOf(total)), String.valueOf(orderId), baseUrl);
+            return "redirect:" + vnpayUrl;
+        }
 
         redirectAttributes.addFlashAttribute(orderRepository.findAll().get(orderRepository.findAll().size() - 1).getOrderId());
-        sessionService.remove("cart");
+
         sessionService.remove("itemNumber");
-        cartItemRepository.deleteAll();
-        cartRepository.deleteAll();
         return "redirect:/confirmation";
     }
 }
