cabae410d21ef538a97fc5d6b797066a0438af83
NgvLanh <130779744+NgvLanh@users.noreply.github.com>
2024-06-15 10:20:31 +0700

    Update CartItemRepository.java

--- src/main/java/com/poly/repositories/CartItemRepository.java
+++ src/main/java/com/poly/repositories/CartItemRepository.java
@@ -5,6 +5,9 @@ import com.poly.entities.CartItem;
 import com.poly.entities.ProductItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     CartItem findByCartAndProductItem(Cart cart, ProductItem productItem);
+    List<CartItem> findCartItemByCartCartId(Long cartId);
 }
