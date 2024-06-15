8c799d8c86b51fd708e17c75706b113c13c52c00
NgvLanh <130779744+NgvLanh@users.noreply.github.com>
2024-06-15 10:20:47 +0700

    Update CartRepository.java

--- src/main/java/com/poly/repositories/CartRepository.java
+++ src/main/java/com/poly/repositories/CartRepository.java
@@ -4,4 +4,5 @@ import com.poly.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    Cart findCartByCustomerCustomerId(Long customerId);
 }
